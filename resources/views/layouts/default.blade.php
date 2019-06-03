5d5674e6ebdc4af34e42686e306bc60c1ee4a37f
whs5280 <13434118350@163.com>
2019-06-03 11:11:41 +0800

    微博删除功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,8 +12,8 @@
 
     <div class="container">
         <div class="offset-md-1 col-md-10">
-            @yield('content')
             @include('shared.messages')
+            @yield('content')
             @include('layouts.footer')
         </div>
     </div>
